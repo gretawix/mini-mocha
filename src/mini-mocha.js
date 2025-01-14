947466fd08a65dff78c16da58a4eb1185a9f9bca
gretawix <gretav@wix.com>
2025-01-14 17:41:55 +0200

    first 4 tests

--- src/mini-mocha.js
+++ src/mini-mocha.js
@@ -1,11 +1,41 @@
-let count = 0;
+let passCount = 0;
+let failCount = 0;
+let level = 0;
 
-global.it = function(description, fn) {
-  fn();
-  console.log(`  ✓ ${description}`);
-  count++;
+const indent = () => "  ".repeat(level + 1);
+
+global.it = function (description, fn) {
+    let errorMessage = "";
+
+    const logDescription = (symbol, { comma } = {}) => {
+        console.log(`${indent()}${symbol} ${description}${comma ? ":" : ""}\n`);
+    };
+
+    try {
+        fn();
+        logDescription("✓");
+        passCount++;
+    } catch (err) {
+        errorMessage = err.toString();
+        failCount++;
+        logDescription(`${failCount})`);
+    } finally {
+        console.log(`  ${passCount} passing`);
+        if (failCount > 0) {
+            console.log(`${indent()}${failCount} failing\n`);
+            logDescription(`${failCount})`, { comma: true });
+            console.log(`${indent().repeat(3)}${errorMessage}`);
+        }
+    }
+
+    return { passCount, failCount };
+};
+
+global.describe = function (suite, fn) {
+    console.log(`${indent()}${suite}`);
+    level++;
+    fn();
+    level--;
 };
 
 require(process.argv[2]);
-console.log('');
-console.log(`  ${count} passing`);
