96b6d0f983f5d6ecf0ed83a372bc388fdec8d7c6
gretawix <gretav@wix.com>
2025-01-15 12:14:36 +0200

    refactor

--- src/mini-mocha.js
+++ src/mini-mocha.js
@@ -2,33 +2,27 @@ let passCount = 0;
 let failCount = 0;
 let level = 0;
 
+const itResults = [];
+
 const indent = () => "  ".repeat(level + 1);
 
+const log = (text) => console.log(text);
+
+const logDescription = (symbol, description, { end } = {}) => {
+    log(`${indent()}${symbol} ${description}${end ? end : ""}`);
+};
+
 global.it = function (description, fn) {
-    let errorMessage = "";
-
-    const logDescription = (symbol, { comma } = {}) => {
-        console.log(`${indent()}${symbol} ${description}${comma ? ":" : ""}\n`);
-    };
-
     try {
         fn();
-        logDescription("✓");
+        logDescription("✓", description);
         passCount++;
+        itResults.push({ description, pass: true });
     } catch (err) {
-        errorMessage = err.toString();
         failCount++;
-        logDescription(`${failCount})`);
-    } finally {
-        console.log(`  ${passCount} passing`);
-        if (failCount > 0) {
-            console.log(`${indent()}${failCount} failing\n`);
-            logDescription(`${failCount})`, { comma: true });
-            console.log(`${indent().repeat(3)}${errorMessage}`);
-        }
+        logDescription(`${failCount})`, description);
+        itResults.push({ description, pass: false, errorMessage: err.toString() });
     }
-
-    return { passCount, failCount };
 };
 
 global.describe = function (suite, fn) {
@@ -39,3 +33,14 @@ global.describe = function (suite, fn) {
 };
 
 require(process.argv[2]);
+
+log(`\n${indent()}${passCount} passing`);
+if (failCount > 0) {
+    log(`${indent()}${failCount} failing\n`);
+    itResults.forEach((item, index) => {
+        if (!item.pass) {
+            logDescription(`${index + 1})`, item.description, { end: ":" });
+            log(`\n${indent().repeat(3)}${item.errorMessage}`);
+        }
+    });
+}
