04f6f71f3ae166d0971dec989fe1c1c634626a44
gretawix <gretav@wix.com>
2025-01-15 13:09:36 +0200

    update

--- src/mini-mocha.js
+++ src/mini-mocha.js
@@ -1,7 +1,4 @@
-let passCount = 0;
-let failCount = 0;
 let level = 0;
-
 const itResults = [];
 
 const indent = () => "  ".repeat(level + 1);
@@ -12,21 +9,41 @@ const logDescription = (symbol, description, { end } = {}) => {
     log(`${indent()}${symbol} ${description}${end ? end : ""}`);
 };
 
+const runIt = (test) => {
+    test.level = level;
+
+    try {
+        test.fn();
+        logDescription("✓", test.description);
+        test.pass = true;
+    } catch (err) {
+        logDescription(`${level + 1})`, test.description);
+        test.pass = false;
+        test.errorMessage = err.toString();
+    }
+};
+
+const runDescribe = (test) => {
+    console.log(`${indent()}${test.suite}`);
+    level++;
+    test.fn();
+    level--;
+};
+
 global.it = function (description, fn) {
     try {
         fn();
         logDescription("✓", description);
-        passCount++;
-        itResults.push({ description, pass: true });
+        itResults.push({ type: "it", description, fn, level, pass: true });
     } catch (err) {
-        failCount++;
-        logDescription(`${failCount})`, description);
-        itResults.push({ description, pass: false, errorMessage: err.toString() });
+        logDescription(`${level + 1})`, description);
+        itResults.push({ type: "it", description, fn, level, pass: false, errorMessage: err.toString() });
     }
 };
 
 global.describe = function (suite, fn) {
     console.log(`${indent()}${suite}`);
+    itResults.push({ type: "describe", suite, fn, level });
     level++;
     fn();
     level--;
@@ -34,9 +51,21 @@ global.describe = function (suite, fn) {
 
 require(process.argv[2]);
 
-log(`\n${indent()}${passCount} passing`);
-if (failCount > 0) {
-    log(`${indent()}${failCount} failing\n`);
+const sortByLevelAndType = (a, b) => {
+    if (a.level !== b.level) return a.level - b.level; // Sort by level first
+    if (a.type === "it" && b.type === "describe") return -1; // `it` before `describe`
+    if (a.type === "describe" && b.type === "it") return 1; // `describe` after `it`
+    return 0;
+};
+
+const sortedTests = itResults.sort(sortByLevelAndType);
+const passed = sortedTests.filter((item) => item.type === "it" && item.pass);
+const failed = sortedTests.filter((item) => item.type === "it" && !item.pass);
+
+log(`\n${indent()}${passed.length} passing`);
+
+if (failed.length > 0) {
+    log(`${indent()}${failed.length} failing\n`);
     itResults.forEach((item, index) => {
         if (!item.pass) {
             logDescription(`${index + 1})`, item.description, { end: ":" });
